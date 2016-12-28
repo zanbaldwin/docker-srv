c71771c466ccdf3c886851c38f64459372b448b9
Zander Baldwin <hello@zanderbaldwin.com>
2016-12-28 17:56:10 +0000

    Install LetsEncrypt Certificates

---
+++ install-certificate.sh
@@ -0,0 +1,11 @@
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "Please supply a domain for the first argument (without www)." 1>&2
+    exit 1
+fi
+
+docker exec nginx nginx -t && docker stop nginx && letsencrypt certonly --standalone -d $1 -d www.$1 && docker start nginx
