febfab0215981dc2448e0c69ddee7ca3fd246b3c
Zander Baldwin <hello@zanderbaldwin.com>
2017-01-10 11:46:25 +0000

    Add Image Build Script
    Add a Bash script to build all the images defined in the repository.

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+IMAGE_PREFIX=${IMAGE_PREFIX:-}
+IMAGE_TAG_SUFFIX=${IMAGE_TAG_SUFFIX:-}
+
+DOCKER=$(which docker)
+if [ $? -ne 0 ]; then
+    echo "Docker not installed."
+    exit 1
+fi
+
+INFO=$($DOCKER info)
+if [ $? -ne 0 ]; then
+    echo "Docker not accessible."
+    exit 2
+fi
+
+# Get full, resolved directory of the currect script.
+SOURCE="${BASH_SOURCE[0]}"
+# Resolve $SOURCE until the file is no longer a symlink.
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    # If $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located.
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Determine if the build script is part of a Git repository, and determine the appropriate tag to use for the image.
+IMAGE_TAG="latest"
+GIT=$(which git)
+if [ $? -eq 0 ]; then
+    # Git is installed. Great start.
+    GITDIR=$(cd $DIR; $GIT rev-parse --git-dir 2>/dev/null)
+    if [ "$GITDIR" != "" ]; then
+        # The directory that the current script is located in is a Git repository, grab the latest short tag.
+        IMAGE_TAG=$(cd $DIR; $GIT describe --tags --abbrev=0)
+    fi
+fi
+
+for PATH in $DIR/images/*; do
+    IMAGE=(${PATH//\// })
+    IMAGE=${IMAGE[${#IMAGE[@]}-1]}
+    IMAGE_NAME="${IMAGE_PREFIX}darsyn/${IMAGE}:${IMAGE_TAG}${IMAGE_TAG_SUFFIX}"
+    $DOCKER build -t "$IMAGE_NAME" $PATH
+done
