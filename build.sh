c1acbf44807bd5e497624c9194a56119a47073e3
Zander Baldwin <hello@zanderbaldwin.com>
2017-01-23 11:40:47 +0000

    Tidy Up Build Script

--- build.sh
+++ build.sh
@@ -20,12 +20,12 @@ fi
 SOURCE="${BASH_SOURCE[0]}"
 # Resolve $SOURCE until the file is no longer a symlink.
 while [ -h "$SOURCE" ]; do
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd"
     SOURCE="$(readlink "$SOURCE")"
     # If $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located.
     [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
 # Determine if the build script is part of a Git repository, and determine the appropriate tag to use for the image.
 IMAGE_TAG="latest"
