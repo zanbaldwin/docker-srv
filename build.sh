e8adb9f8ec561931e056d027059ef10fce1b008b
Zan Baldwin <hello@zanderbaldwin.com>
2017-03-17 13:51:19 +0000

    Update Build Script
    Comment all the things!

--- build.sh
+++ build.sh
@@ -1,44 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Dependencies:
+#     Required: bash, dirname, readlink, grep, awk
+#     Optional: id, git
+# A workaround for Bash will hopefully be found in the future as /bin/sh is more
+# widely available on *nix systems.
 
 # Use this if you wish to prefix the image name with something.
-# E.g. "registry.gitlab.com/" to result in images like "registry.gitlab.com/vendor/image:tag".
+# E.g. "registry.gitlab.com/" to result in images like
+# "registry.gitlab.com/vendor/image:tag".
 IMAGE_PREFIX=${IMAGE_PREFIX:-}
 
-DOCKER=$(which docker)
+# If Docker is installed in a non-standard location, set it with the $DOCKER
+# environment variable, eg: `DOCKER=/usr/bin/docker-client ./build.sh`
+DOCKER=${DOCKER:-"docker"}
+# Use the built-in shell "command" to determine executables instead of relying
+# on "which". Much more versatile as you can specify alternative executables not
+# on the $PATH via environmental variables.
+command -v "${DOCKER}" >/dev/null 2>&1 || { echo >&2 "Docker Client \"${DOCKER}\" not installed. Aborting!"; exit 1; }
+# No need to resolve $DOCKER to an absolute path, since "command" has already
+# determined it's an executable.
+
+INFO=$("${DOCKER}" info 1>/dev/null 2>&1)
 if [ $? -ne 0 ]; then
-    echo "Docker not installed."
+    echo "Docker Daemon unavailable. Aborting!"
+    if [ $(id -u 2>/dev/null) -ne 0 ]; then
+        echo "Perhaps retry as root?"
+    fi
     exit 1
 fi
 
-INFO=$(${DOCKER} info 1>/dev/null 2>&1)
-if [ $? -ne 0 ]; then
-    echo "Docker not accessible."
-    if [ $(id -u) -ne 0 ]; then
-        echo "Try rerunning this script as root."
-    fi
-    exit 2
-fi
-
-# Get full, resolved directory of the current script.
-SOURCE="${BASH_SOURCE[0]}"
-# Resolve $SOURCE until the file is no longer a symlink.
-while [ -h "${SOURCE}" ]; do
-    DIR="$(cd -P "$(dirname "${SOURCE}")" && pwd)"
-    SOURCE="$(readlink "${SOURCE}")"
-    # If $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located.
-    [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
-done
-DIR="$(cd -P "$(dirname "${SOURCE}")" && pwd)"
+# Get the full, resolved directory of the current script NOT the current working
+# directory. The following relies on dirname and readlink executables being
+# available on the path, but at least this solution isn't restricted to
+# Bash-only environments.
+DIR=$(dirname "$(readlink -f "$0")")
 
 # Determine if the build script is part of a Git repository, and determine the appropriate tag to use for the image.
 IMAGE_TAG="latest"
-GIT=$(which git)
+GIT=$(command -v git)
 if [ $? -eq 0 ]; then
     # Git is installed. Great start.
-    GITDIR=$(cd ${DIR}; ${GIT} rev-parse --git-dir 2>/dev/null)
+    GITDIR=$(cd "${DIR}"; "${GIT}" rev-parse --git-dir 2>/dev/null)
     if [ "${GITDIR}" != "" ]; then
         # The directory that the current script is located in is a Git repository, grab the latest short tag.
-        IMAGE_TAG=$(cd ${DIR}; ${GIT} describe --tags --abbrev=0)
+        IMAGE_TAG=$(cd "${DIR}"; "${GIT}" describe --tags --abbrev=0)
     fi
 fi
 
@@ -48,7 +53,7 @@ for IMAGE_DIR in ${DIR}/images/*; do
     IMAGE="${IMAGE[${#IMAGE[@]}-1]}"
     IMAGE_NAME="${IMAGE_PREFIX}darsyn/${IMAGE}:${IMAGE_TAG}"
     # Pull latest parent images.
-    PARENT=$(cat "${IMAGE_DIR}/Dockerfile" 2>/dev/null | grep "^FROM\\s.\+\(\\:.\+\)\?$" 2>/dev/null | awk '{print $2}' 2>/dev/null)
+    PARENT=$(grep "^FROM\\s.\+\(\\:.\+\)\?$" "${IMAGE_DIR}/Dockerfile" 2>/dev/null | awk '{print $2}' 2>/dev/null)
     if [ "${PARENT}" != "" ]; then
         "${DOCKER}" pull "${PARENT}"
     fi
