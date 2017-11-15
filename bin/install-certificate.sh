5ae282129785f9b2ded29a5140b36486aa21fead
Zan Baldwin <hello@zanbaldwin.com>
2017-11-15 14:07:56 +0000

    Change Letsencrypt Verification Process
    Use the site's individual webroot for verification instead of a standalone webserver. This allows sites to be verified behind Cloudflare and doesn't require the Nginx container to be stopped (therefore stopping all other sites at the same time).

--- bin/install-certificate.sh
+++ bin/install-certificate.sh
@@ -3,60 +3,31 @@
 SCRIPT=$(basename "$0")
 function display_usage() {
     echo "Usage:" 1>&2
-    echo "    $SCRIPT <domain>" 1>&2
+    echo "    ${SCRIPT} <webroot> <domain>" 1>&2
     echo "" 1>&2
     echo "Arguments:" 1>&2
+    echo "    webroot     The public webroot directory." 1>&2
     echo "    domain      The domain to fetch a certificate for (without www prefix)." 1>&2
     echo "" 1>&2
     echo "Description:" 1>&2
     echo "    Install a certificate from Let's Encrypt for the specified domain." 1>&2
 }
 
-function nginx_control {
-    case $1 in
-        stop)
-            if [ "${NGINX_RUNNING}" != "true" ]; then
-                return
-            fi
-            ${DOCKER} stop ${NGINX_CONTAINER}
-            ;;
-        start)
-            if [ "${NGINX_RUNNING}" != "true" ]; then
-                return true
-            fi
-            ${DOCKER} exec ${NGINX_CONTAINER} nginx -t 1>/dev/null 2>&1
-            if [ $? -ne 0 ]; then
-                echo "Nginx configuration is not valid." 1>&2
-                echo "Run \"[docker exec nginx] nginx -t\" for detailed output." 1>&2
-                echo 1
-            fi
-            ${DOCKER} start ${NGINX_CONTAINER}
-            ;;
-        *)
-            echo "Internal error (invalid Nginx control command)." 1>&2
-            exit 1
-            ;;
-    esac
-}
+if [ $# -lt 2 ]; then
+    display_usage
+    exit 1
+fi
 
 if [ $(id -u) -ne 0 ]; then
     echo "This script must be run as root." 1>&2
     exit 1
 fi
 
-if [ $# -eq 0 ]; then
-    display_usage
+if [ ! -d "$1" ]; then
+    echo "Webroot is not a valid directory." 1>&2
     exit 1
 fi
 
-NGINX_CONTAINER="nginx"
-DOCKER=$(which docker)
-if [ $? -ne 0 ]; then
-    echo "Docker not installed."
-    exit 1
-fi
-# Don't bother checking if user has access to Docker, we already know we are
-# running as root.
-NGINX_RUNNING=$(${DOCKER} inspect -f "{{.State.Running}}" ${NGINX_CONTAINER})
-
-nginx_control stop && letsencrypt certonly --standalone -d "$1" -d "www.$1" && nginx_control start
+LETSENCRYPT=${LETSENCRYPT:-"letsencrypt"}
+command -v "${LETSENCRYPT}" >/dev/null 2>&1 || { echo >&2 "Let's Encrypt client \"${LETSENCRYPT}\" not installed. Aborting!"; exit 1; }
+"${LETSENCRYPT}" certonly --webroot -w "$1" -d "$2" -d "www.$2"
--- bin/renew-certificates.sh
+++ bin/renew-certificates.sh
@@ -1,45 +1,5 @@
 #!/usr/bin/env bash
 
-function nginx_control {
-    case $1 in
-        stop)
-            if [ "${NGINX_RUNNING}" != "true" ]; then
-                return 0
-            fi
-            ${DOCKER} stop ${NGINX_CONTAINER}
-            ;;
-        start)
-            if [ "${NGINX_RUNNING}" != "true" ]; then
-                return 0
-            fi
-            CONFIG=$(${DOCKER} exec ${NGINX_CONTAINER} nginx -t 1>/dev/null 2>&1)
-            if [ $? -ne 0 ]; then
-                echo "Nginx configuration is not valid." 1>&2
-                echo "Run \"[docker exec nginx] nginx -t\" for detailed output." 1>&2
-                echo 1
-            fi
-            ${DOCKER} start ${NGINX_CONTAINER}
-            ;;
-        *)
-            echo "Internal error (invalid Nginx control command)." 1>&2
-            exit 1
-            ;;
-    esac
-}
-
-if [ $(id -u) -ne 0 ]; then
-    echo "This script must be run as root." 1>&2
-    exit 1
-fi
-
-NGINX_CONTAINER="nginx"
-DOCKER=$(which docker)
-if [ $? -ne 0 ]; then
-    echo "Docker not installed."
-    exit 1
-fi
-# Don't bother checking if user has access to Docker, we already know we are
-# running as root.
-NGINX_RUNNING=$(${DOCKER} inspect -f "{{.State.Running}}" ${NGINX_CONTAINER})
-
-nginx_control stop && letsencrypt renew && nginx_control start
+LETSENCRYPT=${LETSENCRYPT:-"letsencrypt"}
+command -v "${LETSENCRYPT}" >/dev/null 2>&1 || { echo >&2 "Let's Encrypt client \"${LETSENCRYPT}\" not installed. Aborting!"; exit 1; }
+"${LETSENCRYPT}" renew
